1b64f8a1849144c99dcedb9dd25786f1d92e8560
RicoHHN <rico.anzer@web.de>
2025-08-19 13:39:26 +0200

    Fixed window titles

--- obj/Debug/net8.0-windows/NoteApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NoteApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823fe65e665e7b56e2dca276fb9c062d3cf9121f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
